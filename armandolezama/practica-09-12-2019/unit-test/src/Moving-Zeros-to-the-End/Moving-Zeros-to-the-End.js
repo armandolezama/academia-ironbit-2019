2350ec368d07e1775853bbd589c04608e46c70c0
armandolezama <victor.lezama@ironbit.com.mx>
2020-01-12 22:17:24 -0600

    All saved

--- armandolezama/practica-09-12-2019/unit-test/src/Moving-Zeros-to-the-End/Moving-Zeros-to-the-End.js
+++ armandolezama/practica-09-12-2019/unit-test/src/Moving-Zeros-to-the-End/Moving-Zeros-to-the-End.js
@@ -1,1 +1,43 @@
-module.exports = arr => [...arr.filter(value => value !== 0), ...arr.filter(value => value === 0)]
+module.exports = arr => [...arr.filter(value => value !== i), ...arr.filter(value => value === 0)]
+
+const findAll = (sum, digits) => {
+    let result = [];
+        if(sum < digits * 9){
+            let lowerValue = Array(digits).fill(1)
+            sum -= digits;
+            if(sum >= 8){
+                let i = digits -1;
+                do{
+                    lowerValue[i] += 8  
+                    i --;
+                    sum -= 8;
+                } while(sum >= 8)
+                if(sum > 0) lowerValue[0] += sum
+            } else {
+                lowerValue[i] += sum;
+            }
+            //Start substracting and adding that value
+            //Save all valid values
+            //Try new combinations by saved values
+        } else if (sum === digits * 9){
+            result = Array(digits).fill(9)
+        }
+    return result;
+}
+
+const getLowerNumber = (sum, digits) => {
+    let lowerValue = Array(digits).fill(1)
+    sum -= digits;
+    if(sum >= 8){
+        let i = digits -1;
+        do{
+            lowerValue[i] += 8  
+            i --;
+            sum -= 8;
+        } while(sum >= 8)
+        if(sum > 0) lowerValue[i] += sum
+    } else {
+        lowerValue[i] += sum;
+    }
+    return lowerValue;
+}
--- armandolezama/practica-13-12-2019/exam.js
+++ armandolezama/practica-13-12-2019/exam.js
@@ -20,3 +20,39 @@ const primeNumbers = (from, to) => {
     })
 };
 
+const sumStrings = (a,b) => {
+    let firstNumber = [];
+    let secondNumber = [];
+    let fatNumber = [];
+    if([...a].length >= [...b].length) {
+        firstNumber = [...a];
+        secondNumber = [...b];
+    } else {
+        firstNumber = [...b];
+        secondNumber = [...a];
+    };
+    
+    let lengthDifference = firstNumber.length - secondNumber.length;
+    for(let i = firstNumber.length - 1; i >= 0; i--){
+      let digitNumber = (parseInt(firstNumber[i]) + parseInt(secondNumber[i - lengthDifference])).toString()
+        if(digitNumber.length > 1 && i > 0){
+            firstNumber[i - 1] = (parseInt(digitNumber[0]) + parseInt(firstNumber[i - 1])).toString();
+            fatNumber = [digitNumber[digitNumber.length - 1],...fatNumber]
+            } else {
+                fatNumber = [digitNumber,...fatNumber]
+            }
+        if(i - lengthDifference === 0 && i !== 0){
+            fatNumber = [...firstNumber.slice(0, i), ...fatNumber]
+            i = 0;
+        }
+
+    }
+    let counter = 0;
+    do{
+    
+
+    }while()
+    return fatNumber.join('').toString()
+  }
+
+  sumStrings('8797', '45')
